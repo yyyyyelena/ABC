1adddba44279738083acdf867b6c9d678139422d
yyyyyelena <qy890@nyu.edu>
2025-09-23 09:02:04 +0800

    New - 9.23 ev

--- Yelenas-Chat/public/script.js
+++ Yelenas-Chat/public/script.js
@@ -1,61 +1,102 @@
 const socket = io();
 
-let userEle = document.querySelector("#newUser");
+// let userEle = document.querySelector("#newUser");
 let userInput = document.querySelector("#newUserName");
-let formEle = document.querySelector("#chatForm");
+// let formEle = document.querySelector("#chatForm");
 let msgInput = document.querySelector("#newMessage");
 
 let chatThreadList = document.querySelector("#messageList");
 let userNameList = document.querySelector("#userList");
 
-formEle.addEventListener("submit", newMessagesSubmitted);
-userEle.addEventListener("submit",newUserSubmitted);
+document.querySelector("#chatForm").addEventListener("submit", newMessagesSubmitted);
+// document.querySelector("#newUser").addEventListener("submit",newUserSubmitted);
+
 
 function newMessagesSubmitted(event){
-  // console.log(event);
-
-  //to print the message and stop form element from refreshing the page
   event.preventDefault();
 
   let newMsg = msgInput.value;
- 
-  appendMessage(newMsg);
-  socket.emit("message",newMsg);
-
-  msgInput.value = "";
-}
-
-function newUserSubmitted(userEvent){
-
-  //to print the message and stop form element from refreshing the page
-  userEvent.preventDefault();
-
   let newUser = userInput.value;
- 
-  appendUser(newUser);
-  socket.emit("user",newUser);
 
-  newUser.value = "";
+  let packet = {
+    sender: newUser,
+    msg: newMsg
+  }
+  console.log("sending to server:", packet)
+ 
+  socket.emit("message-from-client",packet);
+  // send the message(which is in the packet) from client to server
+  msgInput.value = "";
+  // clear the text box
+
 }
 
-function appendMessage(txt) {
+
+// listen the message(packet) from the server
+socket.on("message-from-server", function(data){
+  appendMessage(data);
+})
+
+
+// function newUserSubmitted(userEvent){
+//   //to print the message and stop form element from refreshing the page
+//   userEvent.preventDefault();
+
+//   let newUser = userInput.value;
+ 
+//   appendUser(newUser);
+//   socket.emit("user",newUser);
+
+//   newUser.value = "";
+// }
+
+  // socket.on("newMessage", appendMessage(newMsg))
+
+  // socket.on("newUserName",appendUser(newUser))
+
+
+
+function appendMessage(data) {
   let chatThreadList = document.querySelector("#threadWrapper ul");
-  console.log(chatThreadList);
-  let newListItem = document.createElement("li")
-  newListItem.innerText = txt;
-  //append new li to the list
-  chatThreadList.append(newListItem);
-  //scroll to the buttom
+
+  let newUserAndMessage = document.createElement("li");
+
+  let userSpan = document.createElement("span");
+  userSpan.classList.add("who");
+  userSpan.innerText = data.sender;
+
+  let msgSpan = document.createElement("span");
+  msgSpan.classList.add("words");
+  msgSpan.innerText = data.msg;
+
+  newUserAndMessage.append(userSpan, document.createTextNode(": "), msgSpan); // this appends to the "li"
+  chatThreadList.append(newUserAndMessage); //this appends to the "ul"
   chatThreadList.scrollTop = chatThreadList.scrollHeight;
 
+  // we dont want to do this in the funciton
+  // because it creates a listener, and we only need to create a listeneer once
+    // socket.on("newMsg", function (data) {
+    // console.log(data)
+    // })
+
 }
 
-function appendUser(name){
-  // console.log(name);
-  let userNameList = document.querySelector("#userWrapper ul");
-  let newUserItem = document.createElement("li")
-  newUserItem.innerText = name;
-  userNameList.append(newUserItem);
-  userNameList.scrollTop = userNameList.scrollHeight;
-}
+// function appendUser(name){
+//   console.log(name);
+//   let userNameList = document.querySelector("#userWrapper ul");
+//   console.log(userNameList)
+//   let newUserItem = document.createElement("li")
+//   newUserItem.innerText = name;
+//   userNameList.append(newUserItem);
+//   userNameList.scrollTop = userNameList.scrollHeight;
+
+//   // we dont want to do this in the funciton
+//   // because it creates a listener, and we only need to create a listeneer once
+
+//   // socket.on("user", function (data) {
+//   //   console.log(data)
+//   //   })
+// }
+
+
 
--- Yelenas-Chat/server.js
+++ Yelenas-Chat/server.js
@@ -2,27 +2,30 @@ const express = require('express');
 const https = require("https");
 const fs = require("fs");
 
-const app = express(); 
+const app = express();
 const portHTTPS = 3001; // port for https
 
-// returning to the client anything that is
-// inside the public folder
-app.use(express.static('public')); 
-// our front end should be in the public folder
+
+app.use(express.static('public'));
 
 
 // to unpack json
-const bodyParser = require('body-parser')//add this
+const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
 
-// Creating object of key and certificate
-// for SSL
 const options = {
-    key: fs.readFileSync("keys-for-local-https/localhost-key.pem"),
-    // this keys-for-local-https was generated automatically with my terminal
-    cert: fs.readFileSync("keys-for-local-https/localhost.pem"),
+  key: fs.readFileSync("keys-for-local-https/localhost-key.pem"),
+  cert: fs.readFileSync("keys-for-local-https/localhost.pem"),
 };
+const HTTPSserver = https.createServer(options, app);
+
+const { Server } = require('socket.io') // include lib
+const io = new Server(HTTPSserver) // start socket io
+
+HTTPSserver.listen(portHTTPS,function(req,res){
+  console.log("HTTPS server started at the port",portHTTPS);
+})
 
 
 
@@ -37,49 +40,45 @@ app.get('/zyx', (req, res) => {
 });
 
 
-// Creating https server by passing
-// options and app object
-// http.createServer(app).listen(portHTTP, function (req, res) {
-//     console.log("HTTP Server started at port", portHTTP);
-// });
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
 
-const HTTPSserver = https.createServer(options, app);
+  socket.on("message-from-client", function(data){
+    console.log(data);
 
-const{Server} = require('socket.io')
-const io = new Server(HTTPSserver)
+    io.emit("message-from-server", data);
 
-io.on('connection',(socket)=>{
-  // we match the connection inside here
-  console.log('a user connected',socket.id);
-
-  socket.on("message",function(incomingMessage){
-    console.log("got new msg:", incomingMessage)
   })
 
-  let messageToAllClients = {
-    sender:"unknown"
-    // message: "incomingMessage"
-  }
+
+  // socket.on("message", function (incomingMessage) {
+  //   console.log("got new msg:", incomingMessage)
+
+  //   let messageToAllClients = {
+  //     sender: "unknown",
+  //     message: "incomingMessage"
+  //   }
+  //   io.emit("newMsg", messageToAllClients);
+  // })
+
+  // socket.on("user",function (incomingUser){
+  //   console.log("got new uswer:",incomingUser)
+
+  //   let messageToAllClients = {
+  //     sender: "unknown",
+  //     message: "incomingUser"
+  //   }
+  //   io.emit("newUser",messageToAllClients);
+
+  // })
 
 
-  socket.on("disconnect", function(){
-    console.log('a user disconnect',socket.id);
+
+  socket.on("disconnect", () => {
+    console.log('a user disconnect', socket.id);
   })
 
 })
-
-
-HTTPSserver.listen(portHTTPS,function(req,res){
-  console.log("HTTPS Server started at port", portHTTPS);
-})
-
-// this can be separated into 2, now it is both creating and listening 
-// https.createServer(options, app).listen(portHTTPS, function (req, res) {
-//     console.log("HTTPS Server started at port", portHTTPS);
-// });
-
-
-
 
 
 
